e8e62676090f0d6c05c5f9389d77ef8417168035
bprigent <hello@bprigent.com>
2023-10-10 20:14:33 -0400

    update server to have correct db + middleware

--- server/index.js
+++ server/index.js
@@ -1,10 +1,57 @@
 // server/index.js
-
-const express = require("express");
+const express = require('express');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const db = require('./db');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
+
+// express-session middleware
+app.use(
+    session({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+// Register endpoint
+app.post('/register', async (req, res) => {
+  try {
+      const { username, password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = { username, password: hashedPassword };
+      db.users.push(user);
+      req.session.userId = user.username;
+      res.status(201).send();
+  } catch (error) {
+      res.status(500).send();
+  }
+});
+
+// Login endpoint
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = db.users.find((u) => u.username === username);
+
+  if (user && await bcrypt.compare(password, user.password)) {
+      req.session.userId = user.username;
+      res.send();
+  } else {
+      res.status(401).send();
+  }
+});
+
+// Logout endpoint
+app.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+      if (err) return res.status(500).send();
+      res.send();
+  });
+});
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server nodemon changes!" });
