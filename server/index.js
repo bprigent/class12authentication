582ba93ac367c31c16b39158786f0a1a9e420ccc
bprigent <hello@bprigent.com>
2023-10-10 20:21:09 -0400

    changes username to email for login

--- server/index.js
+++ server/index.js
@@ -21,11 +21,11 @@ app.use(
 // Register endpoint
 app.post('/register', async (req, res) => {
   try {
-      const { username, password } = req.body;
+      const { username, email, password } = req.body;
       const hashedPassword = await bcrypt.hash(password, 10);
-      const user = { username, password: hashedPassword };
+      const user = { username, email, password: hashedPassword };
       db.users.push(user);
-      req.session.userId = user.username;
+      req.session.userId = user.email;
       res.status(201).send();
   } catch (error) {
       res.status(500).send();
@@ -34,11 +34,11 @@ app.post('/register', async (req, res) => {
 
 // Login endpoint
 app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  const user = db.users.find((u) => u.username === username);
+  const { email, password } = req.body;
+  const user = db.users.find((u) => u.email === email);
 
   if (user && await bcrypt.compare(password, user.password)) {
-      req.session.userId = user.username;
+      req.session.userId = user.email;
       res.send();
   } else {
       res.status(401).send();
